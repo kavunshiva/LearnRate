d1bae0315252e2d7b180a1f7850fc1eeabc06193
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-05-08 21:26:14 -0400

    add simple login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def create
+      user = User.new(user_params).save
+      redirect_to root_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :lessons
   resources :reviews
   resources :users
+  resources :sessions
 
-  root "welcome#index"
+  root "sessions#new"
   resources :welcome, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
