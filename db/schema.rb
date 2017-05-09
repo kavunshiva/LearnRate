b29f39fbde6c40afe0e0a3146a330f94f7052bb4
kavunshiva <kavunshiva@gmail.com>
2017-05-08 22:24:05 -0400

    write seeds script for lessons from learn-students-co GitHub account

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170508200445) do
     t.string "name"
     t.string "description"
     t.string "unit_location"
-    t.string "type"
+    t.string "lesson_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'HTTParty'
+
+url = 'https://api.github.com/organizations/8825476/repos?page='
+
+def get_data(url)
+  resp = HTTParty.get(url)
+  resp.each do |repo|
+    Lesson.create(name: repo["name"])
+  end
+end
+
+for i in 1..50
+  get_data(url+i.to_s)
+end
