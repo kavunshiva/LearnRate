83e3b368e36e8084f9ed580f6e9dcf8c8d6487f7
kavunshiva <kavunshiva@gmail.com>
2017-05-09 15:30:05 -0400

    ignore schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20170508200445) do
     t.string "unit_location"
     t.string "lesson_type"
     t.string "url"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
