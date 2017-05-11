0fb361a449d45087eb697451f0a3036a2ea621a5
Mike Merin <mikemerinweather@gmail.com>
2017-05-11 15:42:07 -0400

    Tag index, Sliders Done

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,12 +2,23 @@ class Rating < ApplicationRecord
 
   belongs_to :review
   # has_one :user, through: :review
-  include RatingsHelper
 
   attr_accessor :time_taken_minutes, :time_taken_hours
 
   def hours_minutes_to_minutes
     self.time_taken_minutes.to_i + (self.time_taken_hours.to_i * 60)
+  end
+
+  def to_hours_minutes
+    "#{to_only_hours}H #{to_only_minutes}M"
+  end
+
+  def to_only_hours
+    self.time_taken / 60
+  end
+
+  def to_only_minutes
+    self.time_taken % 60
   end
 
 end
